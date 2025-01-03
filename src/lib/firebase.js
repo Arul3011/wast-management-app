cc58d10861b1dc49f9693c7b735f73c886e2b0b5
arul <153939531+Arul3011@users.noreply.github.com>
2025-01-03 09:01:23 +0000

    clear errro in firebase while depeloying

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -13,7 +13,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
 const storage = getStorage(app);
-
+const analytics = app.name && typeof window !== 'undefined' ? getAnalytics(app) : null;
 export { storage };
